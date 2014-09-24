734ef47e897ffc65895ca317d36535e761b95e2d
Matt Fornaciari <mattforni@gmail.com>
2014-09-24 00:14:28 -0700

    Updated the rest of the app to play nice with new Message

--- src/com/example/sockettest/network/input/Deserializer.java
+++ src/com/example/sockettest/network/input/Deserializer.java
@@ -1,6 +1,5 @@
 package com.example.sockettest.network.input;
 
-import static com.example.sockettest.network.output.Serializer.ID_KEY;
 import static java.lang.String.format;
 
 import java.util.List;
@@ -12,15 +11,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public abstract class Deserializer {
-    public static final String clientId(final JsonObject data) {
-        try {
-            return data.get(ID_KEY).getAsString();
-        } catch (IllegalStateException e) {
-            Log.w("Deserializer", format("Unable to parse id from %s", data));
-        }
-        return null;
-    }
-
     public static final Song parseSong(final JsonElement serializedSong) {
         try {
             return Song.parse(serializedSong.getAsJsonObject());
--- src/com/example/sockettest/network/input/InputMessage.java
+++ src/com/example/sockettest/network/input/InputMessage.java
@@ -1,23 +1,16 @@
 package com.example.sockettest.network.input;
 
-import static com.example.sockettest.network.output.Serializer.CODE_KEY;
 import static com.example.sockettest.utils.Logger.tag;
-import static java.lang.String.format;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 import android.util.Log;
 
 import com.example.sockettest.Device;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.example.sockettest.network.message.Message;
 
-public abstract class InputMessage {
-    protected static final ByteBuffer BUFFER = ByteBuffer.allocate(1);
-    protected static final JsonParser JSON = new JsonParser();
-
+public abstract class InputMessage extends Message {
     public abstract void receive(final Device device);
 
     protected char nextChar(final SocketChannel channel) {
@@ -29,19 +22,14 @@ public abstract class InputMessage {
         return BUFFER.getChar();
     }
 
-    public static final InputMessage getMessage(final String data) {
-        final JsonObject jsonObject = JSON.parse(data).getAsJsonObject();
-        final int code = jsonObject.get(CODE_KEY).getAsInt();
-        switch(code) {
-            case 1:
-                return new ReceiveClientId(Deserializer.clientId(jsonObject));
-            case 2:
-            	return new ReceiveLibrary(Deserializer.parseLibrary(jsonObject));
-            case 4:
-                return new ReceiveCurrentSong(Deserializer.parseSong(jsonObject));
-            default:
-                Log.e("InputMessage", format("Unrecognized message code received: %d", code));
-                return null;
-        }
+    @Override
+    public void publish(SocketChannel channel) {
+        // TODO This is a placeholder until all messages are moved
+    }
+
+    @Override
+    public String serialize() {
+        // TODO This is a placeholder until all messages are moved
+        return null;
     }
 }
